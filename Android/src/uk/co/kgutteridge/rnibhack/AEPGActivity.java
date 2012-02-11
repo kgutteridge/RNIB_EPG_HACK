7e68ccf6b4b6607428cefbe289cf4ff695fd3b0f
Kieran Gutteridge <kgutteridge@Kierans-MacBook-Pro.local>
2012-02-11 17:43:52 +0000

    feed

--- Android/src/uk/co/kgutteridge/rnibhack/AEPGActivity.java
+++ Android/src/uk/co/kgutteridge/rnibhack/AEPGActivity.java
@@ -19,12 +19,58 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.google.gson.Gson;
 
+import uk.co.kgutteridge.rnibhack.EPGModel.*;
 
 public class AEPGActivity extends Activity {
+	
+	
+	 String url = "http://epgservices.sky.com/tvlistings-proxy/TVListingsProxy/init.json";
+	  
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        InputStream source = retrieveStream(url);
+        
+        Gson gson = new Gson();
+        
+        Reader reader = new InputStreamReader(source);
+        
+        ChannelResponse response = gson.fromJson(reader, ChannelResponse.class);  
     }
+    
+    private InputStream retrieveStream(String url) {
+    	
+    	DefaultHttpClient client = new DefaultHttpClient(); 
+        
+        HttpGet getRequest = new HttpGet(url);
+          
+        try {
+           
+           HttpResponse getResponse = client.execute(getRequest);
+           final int statusCode = getResponse.getStatusLine().getStatusCode();
+           
+           if (statusCode != HttpStatus.SC_OK) { 
+              Log.w(getClass().getSimpleName(), "Error " + statusCode + " for URL " + url); 
+              return null;
+           }
+
+           HttpEntity getResponseEntity = getResponse.getEntity();
+           return getResponseEntity.getContent();
+           
+        } 
+        catch (IOException e) {
+           getRequest.abort();
+           Log.w(getClass().getSimpleName(), "Error for URL " + url, e);
+        }
+        
+        return null;
+        
+     }
+    
+    
 }
