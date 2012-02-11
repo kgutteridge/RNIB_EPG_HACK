256883559931472f2a6e52f5a0d01e9275d901b5
Kieran Gutteridge <kgutteridge@Kierans-MacBook-Pro.local>
2012-02-11 17:28:37 +0000

    Channel Response

--- Android/src/uk/co/kgutteridge/rnibhack/EPGModel/ChannelResponse.java
+++ Android/src/uk/co/kgutteridge/rnibhack/EPGModel/ChannelResponse.java
@@ -1,6 +1,19 @@
 package uk.co.kgutteridge.rnibhack.EPGModel;
 
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+import  uk.co.kgutteridge.rnibhack.EPGModel.*;
+
 public class ChannelResponse
 {
-
+	List <Channel> 			channelsList;
+	List <Epggenre> 		epgGenreList;
+	List <Filters> 			filterList;
+	List <Genre>			genreList;
+	List <RatingsText>		ratingsTextList;
+	List <SelectedRegion>	ratingsList;
+	List <StaticContent>	staticContent;
+	
+	
 }
