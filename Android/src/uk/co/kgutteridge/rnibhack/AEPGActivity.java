b02703b588a4300e212a59ea316c5a98fe1add24
Kieran Gutteridge <kgutteridge@Kierans-MacBook-Pro.local>
2012-02-11 17:32:06 +0000

    Channel Response + Tidy

--- Android/src/uk/co/kgutteridge/rnibhack/AEPGActivity.java
+++ Android/src/uk/co/kgutteridge/rnibhack/AEPGActivity.java
@@ -2,6 +2,23 @@ package uk.co.kgutteridge.rnibhack;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+
 
 public class AEPGActivity extends Activity {
     /** Called when the activity is first created. */
--- Android/src/uk/co/kgutteridge/rnibhack/EPGModel/Filters.java
+++ Android/src/uk/co/kgutteridge/rnibhack/EPGModel/Filters.java
@@ -1,6 +1,13 @@
 package uk.co.kgutteridge.rnibhack.EPGModel;
 
+import com.google.gson.annotations.SerializedName;
+
+
 public class Filters
 {
-
+	@SerializedName("k")
+	public String k;
+	
+	@SerializedName("v")
+	public String v;
 }
