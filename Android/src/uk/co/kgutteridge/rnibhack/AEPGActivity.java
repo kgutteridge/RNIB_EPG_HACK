2e023904ff21e67a55ef7a554921bc56dfe29524
Kieran Gutteridge <kgutteridge@Kierans-MacBook-Pro.local>
2012-02-11 16:29:00 +0000

    Initial project

---
+++ Android/src/uk/co/kgutteridge/rnibhack/AEPGActivity.java
@@ -0,0 +1,13 @@
+package uk.co.kgutteridge.rnibhack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AEPGActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
